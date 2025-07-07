3f03f1863310a60d120cb0cb47d8058f599fb13c
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2025-07-07 21:45:36 +0300

    Counter

--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -3,6 +3,7 @@ package course.concurrency.m2_async.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -16,13 +17,17 @@ public class AsyncClassTest {
     public ApplicationContext context;
 
     @Autowired
+    @Lazy
+    private AsyncClassTest test;
+
+    @Autowired
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
     @Async("applicationTaskExecutor")
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        //internalTask();
+        test.internalTask();
     }
 
     @Async("applicationTaskExecutor")
--- src/main/java/course/concurrency/m3_shared/Counter.java
+++ src/main/java/course/concurrency/m3_shared/Counter.java
@@ -2,22 +2,43 @@ package course.concurrency.m3_shared;
 
 public class Counter {
 
-    public static void first() {
+    private static int count = 1;
+    private static final Object lock = new Object();
 
+    private static void printCount(int c) {
+        synchronized (lock) {
+            while (true) {
+                while (count != c) {
+                    try {
+                        lock.wait();
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(count);
+                count = count % 3 + 1;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    public static void first() {
+        printCount(1);
     }
 
     public static void second() {
-
+        printCount(2);
     }
 
     public static void third() {
-
+        printCount(3);
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> first());
-        Thread t2 = new Thread(() -> second());
-        Thread t3 = new Thread(() -> third());
+        Thread t1 = new Thread(Counter::first);
+        Thread t2 = new Thread(Counter::second);
+        Thread t3 = new Thread(Counter::third);
         t1.start();
         t2.start();
         t3.start();
